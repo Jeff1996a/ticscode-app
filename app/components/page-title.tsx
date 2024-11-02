9056a79199371f2daa09d2e9a0e21928f2275a44
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-01 22:37:16 -0500

    Publicacion

--- app/components/page-title.tsx
+++ app/components/page-title.tsx
@@ -1,13 +1,16 @@
 "use client;"
 import { Typography } from "@material-tailwind/react";
-
+interface TeamCardPropsType {
+  heading: string;
+  children: string;
+}
 export function PageTitle({ heading, children }) {
   return (
     <div   className="mx-auto w-full px-4 text-center lg:w-6/12 ">
-      <Typography variant="h2" color="green" className="mb-3">
+      <Typography variant="h2" color="green" className="mb-3" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
         {heading}
       </Typography>
-      <Typography variant="lead" className="text-blue-gray-500">
+      <Typography variant="lead" className="text-blue-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
         {children}
       </Typography>
     </div>
