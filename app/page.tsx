e31a5b16e90036226f265741fdbef058caf54411
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-06 18:59:52 -0500

    PUblicacion final hasta servicios

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { inter } from '@/app/components/ui/font';
 import { Button } from "@material-tailwind/react";
-import Index from "@/app/index/page"
+import Index from "@/app/home/page"
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 export default function Home() {
   return (
