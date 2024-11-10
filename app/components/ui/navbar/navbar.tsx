06868cb772d670c20d32e07401b9f33447710c37
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-09 22:28:56 -0500

    botones de login y registro

--- app/components/ui/navbar/navbar.tsx
+++ app/components/ui/navbar/navbar.tsx
@@ -337,7 +337,7 @@ const navListItems = [
   {
     label: "Blog",
     icon: RocketLaunchIcon,
-    url:"/home/blog"
+    url:"/blog"
   },
   {
     label: "Contactos",
@@ -413,12 +413,12 @@ export function ComplexNavbar() {
           <button
             className="hidden px-4 py-2 font-sans text-xs font-bold text-center text-blue-500 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-green-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
             type="button">
-            <span>Sign in</span>
+            <Link href="/register"><span>Sign in</span></Link>
           </button>
           <button
             className="hidden select-none rounded-lg bg-gradient-to-tr from-light-green-500 to-light-green-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
             type="button">
-            <span>Log in</span>
+            <Link href="/login"><span>Log in</span></Link>
           </button>
         </div>
       </div>
