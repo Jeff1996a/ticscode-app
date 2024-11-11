55b48e629ba25ff07f07949c81d9a40c9c17ff32
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-11 14:59:33 -0500

    register form

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -13,12 +13,12 @@ import {
    
   export default function RegisterCard() {
     return (
-      <div className="relative w-full flex h-screen content-center items-center justify-center pt-16 pb-32">
-        <Card className="w-96 border border-green-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+      <div className="relative w-full flex sm:mt-60 md:mt-36 h-screen content-center items-center justify-center pt-16 pb-32">
+        <Card className="w-2/4 border border-green-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
           <CardHeader
                   variant="gradient"
                   color="white"
-                  className="mb-2 grid h-28 place-items-center border border-green-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+                  className="mb-1 grid h-28 place-items-center border border-green-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
             <Typography variant="h3" color="white" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
             <Image
                             className="h-100 w-100 mt-0 mb-0 mx-auto object-cover object-center opacity-90"
@@ -37,30 +37,67 @@ import {
                           className="ml-1 font-bold text-center" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
                 Crear cuenta
               </Typography>
-            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-              <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:cols-span-3 w-full max-w-sm min-w-[200px]">
-                  <label className="block mb-2 text-sm text-slate-600">
-                    Nombres
-                  </label>
-                  <input type="text" className="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow" placeholder="Your Name" />
+              <form className="mt-2 mb-1 w-full">
+                <div className="grid grid-cols-1 gap-y-2 md:grid-cols-2 md:gap-x-2 md:grid-cols-2">
+                  <Input label="Nombres" size="lg" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                  <Input label="Apellidos" size="lg" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                  <div className="md:col-span-2 w-full">
+                    <Input  label="Email" size="lg" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                  </div>
+                  <Input type="password" label="Contraseña" size="lg" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                  <Input type="password" label="Repetir contraseña" size="lg" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                  <div className="md:col-span-1">
+                    
+                    <div className="mt-0">
+                    <label className="block text-sm text-blue-gray-800 ml-1">
+                        Pais
+                    </label>
+                    <select
+                          className="w-full bg-transparent placeholder:text-gray-400 text-blue-gray-500 text-sm border border-blue-gray-200 rounded pl-3 pr-8 py-3 transition duration-300 ease focus:outline-none focus:border-gray-400 hover:border-gray-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+                          <option value="brazil">Brazil</option>
+                          <option value="bucharest">Bucharest</option>
+                          <option value="london">London</option>
+                          <option value="washington">Washington</option>
+                      </select>
+                    
+                    </div>
+                  </div>
+                  <div className="md:col-span-1">
+                    
+                    <div className="mt-0">
+                    <label className="block text-sm text-blue-gray-800 ml-1">
+                        Ciudad/Provincia
+                    </label>
+                    <select
+                          className="w-full bg-transparent placeholder:text-gray-400 text-blue-gray-500 text-sm border border-blue-gray-200 rounded pl-3 pr-8 py-3 transition duration-300 ease focus:outline-none focus:border-gray-400 hover:border-gray-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+                          <option value="brazil">Brazil</option>
+                          <option value="bucharest">Bucharest</option>
+                          <option value="london">London</option>
+                          <option value="washington">Washington</option>
+                      </select>
+                    
+                    </div>
+                  </div>
+                  <div className="md:col-span-2 w-full">
+                    <Input  label="Dirección" size="lg" onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined} />
+                  </div>
                 </div>
-              </div>
-              <Checkbox
-                label={<Typography
-                  variant="small"
-                  color="gray"
-                  className="flex items-center font-normal" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                >
-                  I agree the
-                  <a
-                    href="#"
-                    className="font-medium transition-colors hover:text-gray-900"
-                  >
-                    &nbsp;Terms and Conditions
-                  </a>
-                </Typography>}
-                containerProps={{ className: "-ml-2.5" }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}              />
-            </form>
+                
+                <Checkbox
+                  label={<Typography
+                    variant="small"
+                    color="gray"
+                    className="flex items-center font-normal" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}                >
+                    I agree the
+                    <a
+                      href="#"
+                      className="font-medium transition-colors hover:text-gray-900"
+                    >
+                      &nbsp;Terms and Conditions
+                    </a>
+                  </Typography>}
+                  containerProps={{ className: "-ml-2.5" }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}              />
+              </form>
           </CardBody>
           <CardFooter className="pt-0" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
             <Button variant="gradient" color="green" fullWidth placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
