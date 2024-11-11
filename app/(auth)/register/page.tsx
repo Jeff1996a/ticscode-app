e01d06dc33dc24ffa463f5c315aaeab682e9290d
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-11 13:28:41 -0500

    register form

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -34,39 +34,7 @@ import {
              ¡Welcome! Enter your details to register.
             </Typography>
             <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-              <div className="mb-1 grid-cols-2 flex flex-col gap-6">
-                <Typography variant="h6" color="blue-gray" className="-mb-3" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-                  Your Name
-                </Typography>
-                <Input
-                  size="md"
-                  placeholder="name@mail.com"
-                  className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                  labelProps={{
-                    className: "before:content-none after:content-none",
-                  }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}                />
-                <Typography variant="h6" color="blue-gray" className="-mb-3" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-                  Your Email
-                </Typography>
-                <Input
-                  size="md"
-                  placeholder="name@mail.com"
-                  className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                  labelProps={{
-                    className: "before:content-none after:content-none",
-                  }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}                />
-                <Typography variant="h6" color="blue-gray" className="-mb-3" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-                  Password
-                </Typography>
-                <Input
-                  type="password"
-                  size="md"
-                  placeholder="********"
-                  className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                  labelProps={{
-                    className: "before:content-none after:content-none",
-                  }} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined} crossOrigin={undefined}                />
-              </div>
+              
               <Checkbox
                 label={<Typography
                   variant="small"
