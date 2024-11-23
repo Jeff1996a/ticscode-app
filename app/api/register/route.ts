81ff1f6fb9502b39b57a9343241949422c8553f1
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-23 15:48:05 -0500

    database connection

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,11 +1,27 @@
 import {NextRequest, NextResponse} from "next/server";
 import { UserType } from "@/app/types/user"
+import {conn} from "@/app/utils/connectiondb"
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await conn.connect();
+      // Send a ping to confirm a successful connection
+      await conn.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await conn.close();
+    }
+}
 
 export async function GET(request: NextRequest){
     const greeting = "Bienvenidos a TicsCode!!"
     const json = {
         greeting
     };
+
+    run().catch(console.dir);
     
     return NextResponse.json(json);
 }
---
+++ app/utils/connectiondb.tsx
@@ -0,0 +1,15 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ticscode:<c0d1n6F0rL1f31996.>@ticscode.cygm3.mongodb.net/?retryWrites=true&w=majority&appName=ticscode";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+export const conn = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
