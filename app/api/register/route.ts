dc3b21798cd7241125d82e09ab8812c45372a9be
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-25 03:53:55 -0500

    load users

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,26 +2,21 @@ import {NextRequest, NextResponse} from "next/server";
 import { UserType } from "@/app/types/user"
 import {conn} from "@/app/utils/connectiondb"
 
-async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await conn.connect();
-      // Send a ping to confirm a successful connection
-      await conn.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await conn.close();
-    }
-}
-
 export async function GET(request: NextRequest){
-    const greeting = "Bienvenidos a TicsCode!!"
-    const json = {
-        greeting
-    };
+    try {
+        await conn.connect();
 
-    run().catch(console.dir);
-    
-    return NextResponse.json(json);
+        // Choose a name for your database
+        const database = conn.db("ticscodeApp");
+
+        // Choose a name for your collection
+        const collection = database.collection("usuario");
+        const allData = await collection.find({}).toArray();
+
+         return NextResponse.json(allData);
+    } catch (error) {
+        return NextResponse.json({ message: "Something went wrong!" });
+    } finally {
+        await conn.close();
+    }
 }
--- app/utils/connectiondb.tsx
+++ app/utils/connectiondb.tsx
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ticscode:<c0d1n6F0rL1f31996.>@ticscode.cygm3.mongodb.net/?retryWrites=true&w=majority&appName=ticscode";
+const uri = "mongodb+srv://ticscode:c0d1n6F0rL1f31996.@ticscode.cygm3.mongodb.net/?retryWrites=true&w=majority&appName=ticscode";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export const conn = new MongoClient(uri, {
