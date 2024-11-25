f4d787c22a2926f526b6f1d88f7236963dbcfd11
Jeff1996a <alfaro.jefferson96@gmail.com>
2024-11-25 17:42:42 -0500

    crud operations

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,8 +1,8 @@
 import {NextRequest, NextResponse} from "next/server";
-import { UserType } from "@/app/types/user"
+import { Usuario } from "@/app/types/usuario"
 import {conn} from "@/app/utils/connectiondb"
 
-export async function GET(request: NextRequest){
+/*export async function GET(request: NextRequest){
 
     try {
         await conn.connect();
@@ -12,13 +12,31 @@ export async function GET(request: NextRequest){
 
         // Choose a name for your collection
         const collection = database.collection("usuario");
-        const allData = await collection.find().toArray();
+        const allData = await collection.find({}).toArray();
 
          return NextResponse.json(allData);
-         
+
     } catch (error) {
         return NextResponse.json({ message: "Something went wrong!" });
     } finally {
         await conn.close();
     }
+}*/
+export default function handler(req, res) {
+    const { method } = req;
+  
+    switch (method) {
+      case 'POST':
+        const data = req.body;
+        // Process the data
+        res.status(200).json({ message: 'Data received', data });
+        break;
+      case 'GET':
+        res.status(200).json({ message: 'This is a GET request' });
+        break;
+      default:
+        res.status(405).json({ error: 'Method not allowed' });
+        break;
+    }
 }
+
